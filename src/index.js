6ef37f2d44944cc017700889122968f55e2ff31e
gnahc2305 <achangmurillo@gmail.com>
2022-10-15 17:22:31 -0400

    setup now works, create header

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+
+const content = document.getElementById('content');
+
+const header = document.createElement('header');
+header.textContent = 'Restaurant';
+
+content.appendChild(header);
